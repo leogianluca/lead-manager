12f9fcbf50844860161da66ac85602d687232d0f
Leonardo Gianluca <leogianluca.costa@gmail.com>
2025-05-28 15:05:05 -0300

    Backend

---
+++ backend/LeadManager.API/Mapping/LeadProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using LeadManager.Application.DTOs;
+using LeadManager.Domain.Entities;
+
+namespace LeadManager.API.Mapping
+{
+    public class LeadProfile : Profile
+    {
+        public LeadProfile()
+        {
+            CreateMap<Lead, LeadDto>()
+               .ForMember(dest => dest.FirstName, opt => opt.MapFrom(src => src.FirstName.Value))
+               .ForMember(dest => dest.LastName, opt => opt.MapFrom(src => src.LastName.Value))
+               .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email.Address))
+               .ForMember(dest => dest.Phone, opt => opt.MapFrom(src => src.Phone.Number))
+               .ForMember(dest => dest.Suburb, opt => opt.MapFrom(src => src.Suburb.Value))
+               .ForMember(dest => dest.Category, opt => opt.MapFrom(src => (int)src.Category))
+               .ForMember(dest => dest.Price, opt => opt.MapFrom(src => src.Price.Value))
+               .ForMember(dest => dest.CreatedAt, opt => opt.MapFrom(src => src.CreatedAt))
+               .ForMember(dest => dest.Status, opt => opt.MapFrom(src => (int)src.Status));
+        }
+    }
+}
---
+++ backend/LeadManager.Application/DTOs/LeadDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeadManager.Application.DTOs
+{
+    public class LeadDto
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Suburb { get; set; }
+        public int Category { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public int Status { get; set; }
+    }
+
+}
